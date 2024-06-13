09ed0fe3604370e185fcd3366bafd9acbb4e4d51
Garry-82 <168720206+Garry-82@users.noreply.github.com>
2024-06-13 11:46:30 +0300

    Add files via upload

--- HomeWork4.py
+++ HomeWork4.py
@@ -1,23 +1,23 @@
 class Building:
-    def __init__(self, total):
-        self.name = total
 
-    def add_buildind(self, n):
-        print(f" Построено {n}-ое здание!!!")
+    total = 0  # счетчик экземпляров класса Building
+    def __init__(self):
+        Building.total += 1
+    def print_info(self,name):
+        self.name = "B" + str(name)
+        print(f" Создан {Building.total}-ый экземпляр класса Building с именем {self.name}!!!")
 
-    def print_info(self):
-        print(f" Поступило задание: необходимо построить {self.name} зданий!!!")
-
-
-building_dict = {}   # - создание 40 объектов типа "Building" в виде словаря
-for i in range(40):
-    building_dict["D" + str(i+1)] = Building(i + 1)
-
-Building(40).print_info()
 print(input("нажмите любую клавишу, чтобы приступить к заданию: "))
 
-for i in range(40):  # - печать построенных объектов
-    building_dict["D" + str(i+1)].add_buildind(i+1)
+building_dict = {}   # - словарь объектов класса Building
+
+for i in range(40):
+    building_dict["B" + str(i+1)] = Building().print_info(Building.total)
+
+
+
+
+
 
 
 
